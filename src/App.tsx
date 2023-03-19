bddf63d66f217e03a04df2ccd44d90793cd45095
Juan Meza <jrmeza514@gmail.com>
2023-03-18 22:03:12 -0700

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      This is not right
+      <button>ASd</button>
+    </div>
+  )
+}
+
+export default App
